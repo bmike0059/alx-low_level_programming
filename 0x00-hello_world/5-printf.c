3523e19bade952d651b7f898078dc13564b9ed0b
Mickyas Balchu <bmike0059@gmail.com>
2022-04-07 08:09:43 -0700

    task 5 done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *My hello, printf task
+ *Return: return 1 after program execution
+ */
+int main(void)
+{
+printf("with proper grammar, but the out come is a piece of art.\n");
+return (0);
+}
