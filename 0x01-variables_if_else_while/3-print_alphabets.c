94b8d5fd8840b5d254b3a7adeab9fc1097f470db
Mickyas Balchu <bmike0059@gmail.com>
2022-04-08 12:04:59 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+for (char x = 'a' ; x <= 'z' ; x++)
+{
+putchar(x);
+}
+for (char y = 'A' ; y<= 'Z' ; y++)
+{
+putchar(y);
+}
+putchar('\n');
+return (0);
+}
